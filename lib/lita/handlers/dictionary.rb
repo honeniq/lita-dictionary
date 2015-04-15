5e683833e40083e358675019ed1e5d6912a466b8
honeniq <honeniq@gmail.com>
2015-04-15 23:04:48 +0900

    Add j2e route

--- lib/lita/handlers/dictionary.rb
+++ lib/lita/handlers/dictionary.rb
@@ -1,6 +1,11 @@
 module Lita
   module Handlers
     class Dictionary < Handler
+      route %r{j2e}, :j2e, command: true, help: {
+        "j2e" => "Translate a japanese word to english"}
+      
+      def j2e()
+      end
     end
 
     Lita.register_handler(Dictionary)
--- spec/lita/handlers/dictionary_spec.rb
+++ spec/lita/handlers/dictionary_spec.rb
@@ -1,4 +1,6 @@
 require "spec_helper"
 
 describe Lita::Handlers::Dictionary, lita_handler: true do
+  it { is_expected.to route_command("j2e").to(:j2e) }
+
 end
